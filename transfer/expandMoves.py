ac4ccd920acd7206ba4240a99d946c54391ba6a4
Colliquake <aroh002@ucr.edu>
2023-02-26 18:40:39 -0800

    fixed crane_moveleft

--- transfer/expandMoves.py
+++ transfer/expandMoves.py
@@ -11,6 +11,7 @@ def expandMoves(self):
 
     if(not(currentlyGrabbing)):
         if(cranePos <= 350):    #CRANE_MOVEUP
+            print("up")
             newGrid = copy.deepcopy(grid)
             newDepth = copy.deepcopy(createGrid.newGrid.getDepth(self))
             newDepth += 1
@@ -22,6 +23,7 @@ def expandMoves(self):
             ret.append(createGrid.newGrid(newGrid, newDepth, newOffContainers, newLoadContainers, newIsGrabbing))
         if(cranePos >= 66):        #CRANE_MOVEDOWN
             if(grid[cranePos - 39][1] == "UNUSED"):
+                print("down")
                 newGrid = copy.deepcopy(grid)
                 newDepth = copy.deepcopy(createGrid.newGrid.getDepth(self))
                 newDepth += 1
@@ -32,6 +34,7 @@ def expandMoves(self):
                 newGrid[cranePos - 39][1] = "CRANE"
                 ret.append(createGrid.newGrid(newGrid, newDepth, newOffContainers, newLoadContainers, newIsGrabbing))
         if((cranePos % 39 != 0) and (grid[cranePos - 1][1] == "UNUSED")):       #CRANE_MOVELEFT
+            print("left")
             newGrid = copy.deepcopy(grid)
             newDepth = copy.deepcopy(createGrid.newGrid.getDepth(self))
             newDepth += 1
@@ -43,6 +46,7 @@ def expandMoves(self):
             ret.append(createGrid.newGrid(newGrid, newDepth, newOffContainers, newLoadContainers, newIsGrabbing))
         if(((cranePos - 38) % 39 != 0)):       #CRANE_MOVERIGHT
             if(grid[cranePos + 1][1] == "UNUSED"):
+                print("right")
                 newGrid = copy.deepcopy(grid)
                 newDepth = copy.deepcopy(createGrid.newGrid.getDepth(self))
                 newDepth += 1
@@ -54,6 +58,7 @@ def expandMoves(self):
                 ret.append(createGrid.newGrid(newGrid, newDepth, newOffContainers, newLoadContainers, newIsGrabbing))
         if(cranePos == 376):
             if(not(createGrid.newGrid.getLoadContainers(self))):
+                print("grab")
                 newGrid = copy.deepcopy(grid)
                 newDepth = copy.deepcopy(createGrid.newGrid.getDepth(self))
                 newDepth += 1
@@ -65,6 +70,7 @@ def expandMoves(self):
                 newLoadContainers.remove(container)
                 ret.append(createGrid.newGrid(newGrid, newDepth, newOffContainers, newLoadContainers, newIsGrabbing))
         elif((grid[cranePos - 39][1] != "UNUSED") and (grid[cranePos - 39][1] != "TRUCK") and (grid[cranePos - 39][1] != "NAN")):       #CRANE_GRAB
+            print("grab")
             newGrid = copy.deepcopy(grid)
             newDepth = copy.deepcopy(createGrid.newGrid.getDepth(self))
             newDepth += 1
@@ -76,6 +82,7 @@ def expandMoves(self):
         return ret
     else:
         if(cranePos <= 350):        #CRANE_MOVEUP
+            print("up")
             newGrid = copy.deepcopy(grid)
             newDepth = copy.deepcopy(createGrid.newGrid.getDepth(self))
             newDepth += 1
@@ -88,6 +95,7 @@ def expandMoves(self):
             ret.append(createGrid.newGrid(newGrid, newDepth, newOffContainers, newLoadContainers, newIsGrabbing))
         if(cranePos >= 105):     #CRANE_MOVEDOWN
             if(grid[cranePos - 78][1] == "UNUSED"):
+                print("down")
                 newGrid = copy.deepcopy(grid)
                 newDepth = copy.deepcopy(createGrid.newGrid.getDepth(self))
                 newDepth += 1
@@ -98,20 +106,23 @@ def expandMoves(self):
                 newGrid[cranePos - 39][1] = "CRANE"
                 newGrid[cranePos - 78][1] = currentlyGrabbing
                 ret.append(createGrid.newGrid(newGrid, newDepth, newOffContainers, newLoadContainers, newIsGrabbing))
-        if((cranePos % 39 != 0) and (grid[cranePos - 1][1] == "UNUSED") and (grid[cranePos - 39 - 1] == "UNUSED")):     #CRANE_MOVELEFT
-            newGrid = copy.deepcopy(grid)
-            newDepth = copy.deepcopy(createGrid.newGrid.getDepth(self))
-            newDepth += 1
-            newOffContainers = copy.deepcopy(createGrid.newGrid.getOffContainers(self))
-            newLoadContainers = copy.deepcopy(createGrid.newGrid.getLoadContainers(self))
-            newIsGrabbing = copy.deepcopy(createGrid.newGrid.getIsGrabbing(self))
-            newGrid[cranePos][1] = "UNUSED"
-            newGrid[cranePos - 39][1] = "UNUSED"
-            newGrid[cranePos - 1][1] = "CRANE"
-            newGrid[cranePos - 39 - 1][1] = currentlyGrabbing
-            ret.append(createGrid.newGrid(newGrid, newDepth, newOffContainers, newLoadContainers, newIsGrabbing))
+        if((cranePos % 39 != 0)):
+            if((grid[cranePos - 1][1] == "UNUSED") and (grid[cranePos - 39 - 1][1] == "UNUSED")):       #CRANE_MOVELEFT
+                print("left")
+                newGrid = copy.deepcopy(grid)
+                newDepth = copy.deepcopy(createGrid.newGrid.getDepth(self))
+                newDepth += 1
+                newOffContainers = copy.deepcopy(createGrid.newGrid.getOffContainers(self))
+                newLoadContainers = copy.deepcopy(createGrid.newGrid.getLoadContainers(self))
+                newIsGrabbing = copy.deepcopy(createGrid.newGrid.getIsGrabbing(self))
+                newGrid[cranePos][1] = "UNUSED"
+                newGrid[cranePos - 39][1] = "UNUSED"
+                newGrid[cranePos - 1][1] = "CRANE"
+                newGrid[cranePos - 39 - 1][1] = currentlyGrabbing
+                ret.append(createGrid.newGrid(newGrid, newDepth, newOffContainers, newLoadContainers, newIsGrabbing))
         if((cranePos - 38) % 39 != 0):      #CRANE_MOVERIGHT
             if((grid[cranePos + 1][1] == "UNUSED") and (grid[cranePos - 39 + 1][1] == "UNUSED")):
+                print("right")
                 newGrid = copy.deepcopy(grid)
                 newDepth = copy.deepcopy(createGrid.newGrid.getDepth(self))
                 newDepth += 1
@@ -125,6 +136,7 @@ def expandMoves(self):
                 ret.append(createGrid.newGrid(newGrid, newDepth, newOffContainers, newLoadContainers, newIsGrabbing))
         if(cranePos == 376):        #CRANE_RELEASE (if crane is above truck)
             if(currentlyGrabbing in createGrid.newGrid.getOffContainers(self)):
+                print("release-- crane/container is currently above truck")
                 newGrid = copy.deepcopy(grid)
                 newDepth = copy.deepcopy(createGrid.newGrid.getDepth(self))
                 newDepth += 1
@@ -136,6 +148,7 @@ def expandMoves(self):
                 ret.append(createGrid.newGrid(newGrid, newDepth, newOffContainers, newLoadContainers, newIsGrabbing))
         elif(grid[cranePos - 78][1] != "UNUSED"):       #CRANE_RELEASE (for all other (valid) scenarios)
             if(((cranePos - 24) % 39 != 0) and ((cranePos - 26) % 39 != 0)):
+                print("release")
                 newGrid = copy.deepcopy(grid)
                 newDepth = copy.deepcopy(createGrid.newGrid.getDepth(self))
                 newDepth += 1
