03509bac2437561f75a796d5f99f6ae3016d8fc0
hbach24 <57569284+hbach24@users.noreply.github.com>
2023-03-01 01:01:12 -0800

    Using Tkinter now. Created 8x12 Grid and a dummy animation on the grid.

--- UI_Han/ButtonGrid.py
+++ UI_Han/ButtonGrid.py
@@ -1,25 +1,67 @@
-import PySimpleGUI as sg
+import tkinter as tk
+import time
 
-sg.theme('Default')
+root = tk.Tk()
+running = True
 
-containers = {}
+# Define a function to start the loop
+def on_start():
+   global running
+   running = True
 
-layout = [[sg.Text('8x12 Ship Grid')]]
+# Define a function to stop the loop
+def on_stop():
+   global running
+   running = False
 
-for row in range(8):
-    new_row = []
-    for col in range(12):
-        new_row.append(sg.Button(size=(5,3), key=(row, col), button_color=("white", "white"), mouseover_colors = ("snow3", "snow3"), pad = (0.5,0.5))) #key=(row,col) represents the grid coordinate for each button 
-        #USE TOOLTIP so user can see container name bigger when hovering over a button/container
-    layout.append(new_row)
-layout.append([sg.Button('Exit')])
+button_grid = []
+def gridSetup():
+    for i in range(8):
+        button_row = []
+        for j in range(12):
+            global button
+            button = tk.Button(root, text="", height=5, width=10, bg='white',activebackground='lightgrey', command=lambda row=i, column=j: print(row, column))
+            button.grid(row=i, column=j)
+            button_row.append(button)
+        button_grid.append(button_row)
 
-window = sg.Window('8x12 Ship Grid', layout, use_default_focus=False)
+def pairChangeButtonColor(pair1, pair2):
+    a, b = pair1
+    print(a, b)
+    c, d = pair2
+    print(c, d)
 
-while True:
-    event, values = window.read()
-    print(event, values)
-    if event in (sg.WIN_CLOSED, 'Exit'):
-        break
+    while True:
+        button1 = button_grid[a][b]
+        button1.config(bg="pink")
+        button1.update()
+        # time.sleep(1)
+        # apparently it's recommended to use .after() instead of time.sleep() bc it could interfere with mainloop(); https://stackoverflow.com/questions/39555463/tkinter-how-to-stop-a-loop-with-a-stop-button
+        root.after(700) 
+        button1.config(bg="white")
+        button1.update()
 
-window.close()
+        button2 = button_grid[c][d]
+        button2.config(bg="pink")
+        button2.update()
+        # time.sleep(1)
+        root.after(700)
+        button2.config(bg="white")
+        button2.update()
+
+        if running == False:  #add condition for when user hits "Next", stop the loop so it can go to next step's new animation
+            break       
+        
+
+def main():
+    gridSetup()
+
+    button = tk.Button(root, text="Next", height=1, width=8, command=on_stop)
+    button.grid(row=9, column=6)
+    pairChangeButtonColor((0,0), (3,3))
+    
+    print("Broke out of animation loop!") #break out of loop from pairChangeButtonColor((0,0), (3,3))
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
