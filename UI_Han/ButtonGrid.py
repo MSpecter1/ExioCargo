fcee1b07029ec0e42ab1a7eba819eab126d4badd
hbach24 <57569284+hbach24@users.noreply.github.com>
2023-03-11 05:11:15 -0800

    Step by step animation seems to be working for ShipCase4; still need to test more ShipCases/Manifests and fix some logic bugs in code

--- UI_Han/ButtonGrid.py
+++ UI_Han/ButtonGrid.py
@@ -4,9 +4,11 @@ import GUIManifestReader
 import numpy as np
 
 root = tk.Tk()
+global running
 running = True
 
 timer = 150
+path_arr = []
 # Define a function to start the loop
 def on_start():
    global running
@@ -16,9 +18,42 @@ def on_start():
 def on_stop():
    global running
    running = False
+   #TODO: call updateNextGrid() in here instead of main()
+
+def pathReader(file):
+    print("PATH READER")
+
+    with open(file) as f:
+        while True:
+                line = f.readline()
+                if not line:
+                    break
+                line=line.strip()
+                # slot1's row
+                slot1_row=int(line[1:3]) 
+                # slot1's col
+                slot1_col=int(line[4:6])
+
+                #slot2's row
+                slot2_row=int(line[8:10])
+                #slot2's col
+                slot2_col=int(line[11:13])
+
+                # Name
+                n=(line[15:])
+
+                global slot1
+                slot1 = (slot1_row, slot1_col)
+                slot2 = (slot2_row, slot2_col)
+
+                path_arr.append((slot1, slot2, n))
+                print("slot1 row,col:", slot1, "// slot2 row,col:", slot2, "Name:", n)
+
+        for i in range(len(path_arr)):
+            print(path_arr[i])
 
 button_grid = []
-containers_arr = GUIManifestReader.read_manifest("ShipCase1.txt")
+containers_arr = GUIManifestReader.read_manifest("ShipCase4.txt")
 def buildShipGrid(): #building a dummy grid
     global containers_2D
     containers_2D = list(np.reshape(containers_arr, (8,12)))
@@ -35,7 +70,22 @@ def buildShipGrid(): #building a dummy grid
             button.grid(row=i, column=j)
             button_row.append(button)
         button_grid.append(button_row)
+    
+    # check button_grid before updateNextGrid() occurs
+    # for i in range(len(button_grid)):
+    #     print("ROW", i)
+    #     for j in range(12):
+    #         print("COL",j, button_grid[i][j].cget("bg"))
 
+    #TEST DELETE LATER
+    # r=1
+    # c=5
+    # testContainer= containers_2D[r][c]
+    # testContainer.button.config(text=testContainer.name + f"({r},{c})", bg='blue', command=lambda row=i, column=j, container_x=testContainer.x, container_y=testContainer.y, container_weight=testContainer.weight, container_name=testContainer.name: print("row:", row, "column:", column, "ManifestX:", container_x, "ManifestY:", container_y, "Name:", container_name, "Weight:", container_weight))
+    # testButton = testContainer.button
+    # testButton.grid(row=r, column=c)
+    # testContainer.button.update()
+    # root.after(800)
 
 def computeRow(y):
     return y + ((-2 * y) + 8)
@@ -44,6 +94,7 @@ def computeCol(x):
     return x-1
 
 def maxY(slot1, slot2):
+    moveUp = 0
     slot1_row, slot1_col = slot1
     print("slot1's row:", slot1_row)
     print("slot1's col:", slot1_col)
@@ -55,45 +106,50 @@ def maxY(slot1, slot2):
     #Condition 1: move left to right; if slot1_col < slot2_col
     if slot1_col < slot2_col:
         print("Condition 1")
-        maxRow = 100
+        maxRow = slot1_row
         for c in range(slot1_col, slot2_col+1): # moving horizontally X
-            for r in range(slot2_row, -1, -1): # moving vertically Y
+            for r in range(8): # moving vertically Y
 
-                print(r,c)
+                # print(r,c)
                 if(button_grid[r][c].cget('bg') != "white" and button_grid[r][c].cget('bg') != "black"): #Find the max Y or row; decrements from slot1's row to the top/0th row
                     # print(button_grid[r][c].cget('bg'), r, c)
                     if(r < maxRow):
                         maxRow = r
-                        # print(maxRow)
-        moveUp = maxRow-1
+                        print("MAXX height/row:", maxRow)
+        moveUp = maxRow-1 # move 1 up above current slot1 container
+
 
     #TODO: Condition 2: move right to left; if slot1_col > slot2_col // here, instead of incrementing the c/col we, decrement it up to slot2_col
     if slot1_col > slot2_col: 
         print("Condition 2")
-        maxRow = 100
+        maxRow = slot1_row
         for c in range(slot1_col, slot2_col-1, -1): # moving horizontally X
-            for r in range(slot2_row, -1, -1): # moving vertically Y
+            for r in range(8): # moving vertically Y
                 # print("r,c", r, c)
                 if(button_grid[r][c].cget('bg') != "white" and button_grid[r][c].cget('bg') != "black"): #Find the max Y or row; decrements from slot1's row to the top/0th row
                     # print(button_grid[r][c].cget('bg'), r, c)
                     if(r < maxRow):
                         maxRow = r
-                        # print(maxRow)
-        moveUp = maxRow-1
-
+                        print("MAXX height/row:", maxRow)
+        moveUp = maxRow-1 # move 1 up above current slot1 container
 
     if(moveUp == 99): #if it's all white space and no containers are blocking in between, just set the max height to row2's row value
         moveUp = slot2_row
+
+    if moveUp > 0: #maxRow>1
+        moveUp = maxRow-1
+    else:
+        moveUp = maxRow
     
     print("Max height/row:", moveUp)
     return moveUp
 
 def animateUp(y, slot1): # combine animateDown here 
     row, col = slot1
-    print("y, row, col", y, row, col)
-
+    # print("y, row, col", y, row, col)
+    
     # while True: #
-    for i in range(row-1, y, -1): # i > y ; onlu up to y to avoid double counting/lighting the maxY button for animateUp
+    for i in range(row-1, y, -1): # i > y ; only up to y to avoid double counting/lighting the maxY button for animateUp
         # print('enter')
         # prevButton = button_grid[i][col]
         lightButton = button_grid[i][col]
@@ -109,9 +165,15 @@ def animateHorizontal(slot1, slot2, moveMaxHeight):
     slot1_row, slot1_col = slot1
     slot2_row, slot2_col = slot2
 
+    
     # Condition 1: move left to right; if slot1_col < slot2_col
     if slot1_col < slot2_col:
-        print("Condition 1: moveRight")
+
+        if moveMaxHeight == slot1_row: #TEMPORARY POSSIBLY: for ShipCase4 case where the container being moved is on the very first row (which is already the maxHeight)
+            slot1_col+=1
+            # print("WAT", slot1_row, slot1_col)
+
+        # print("Condition 1: moveRight")
         for i in range(slot1_col, slot2_col+1):
             lightButton = button_grid[moveMaxHeight][i]
             lightButton.config(bg="blue")
@@ -123,6 +185,9 @@ def animateHorizontal(slot1, slot2, moveMaxHeight):
 
     # Condition 2: move right to left; if slot1_col > slot2_col
     if slot1_col > slot2_col:
+        if moveMaxHeight == slot1_row: #TEMPORARY POSSIBLY: case where the container being moved is on the very first row (which is already the maxHeight), good example of this scenario is ShipCase4
+            slot1_col-=1
+            # print("WAT", slot1_row, slot1_col)
         print("Condition 2: moveLeft")
         for i in range(slot1_col, slot2_col-1, -1):
             lightButton = button_grid[moveMaxHeight][i]
@@ -157,7 +222,8 @@ def animateDown(slot2, moveMaxHeight, moveMaxDown):
         lightButton.config(bg="white")
         lightButton.update()
         root.after(timer)
-    
+
+# TODO perform some updates on updateNextGrid() (information written below)!!!!!
 def updateNextGrid(slot1, slot2): # updates the 8x12 grid to reflect the current step's move/task has been completed once the user presses "Next"
     slot1_row, slot1_col = slot1
     slot2_row, slot2_col = slot2
@@ -170,6 +236,8 @@ def updateNextGrid(slot1, slot2): # updates the 8x12 grid to reflect the current
     updateSlot1Button = button_grid[slot1_row][slot1_col]
     updateSlot1Button.config(bg="white", text="UNUSED"+f"({slot1_row}, {slot1_col})")
     updateSlot1Button.update()
+    #TODO: CHECK IF button_grid IS ALSO UPDATED, NOT ONLY THE GUI GRID RUN FOR LOOP THROUGH BUTTON_GRID AND MAKE A COMPARISON BETWEEN ITS ARRAY CONTENTS AND THEIR POSITIONS BEFORE THE .update() AND AFTER THE .update()
+
 
 def main():
     buildShipGrid()
@@ -182,41 +250,55 @@ def main():
     # pair1 = (2, 10) # (2, 3)
     # pair2 = (3, 12) # (2, 10)
 
-    # pair1 = (2, 3)
-    # pair2 = (2, 10)
-
     pair1 = (1, 3)
     pair2 = (1, 7)
 
-    pair1R, pair1C = pair1
-    pair2R, pair2C = pair2
-    print("pair1", pair1R, pair1C)
-    print("pair2", pair2R, pair2C)
+    pair1 = (8, 5)
+    pair2 = (2, 6)
 
-    slot1_row = computeRow(pair1R) # 6
-    slot1_col = computeCol(pair1C) # 2
-    slot1 = (slot1_row, slot1_col)
+    pathReader("ShipCase4GUIPathOutput.txt")
+    # path_arr = []
+    # path_arr.append((pair1, pair2, "Pig"))
+    for i in range(len(path_arr)):
+        print("\n\nSTEP", i, path_arr[i])
+        slot1R, slot1C = path_arr[i][0]
+        slot2R, slot2C = path_arr[i][1]
+        name = path_arr[i][2]
 
-    slot2_row = computeRow(pair2R) # 6
-    slot2_col = computeCol(pair2C) # 9
-    slot2 = (slot2_row, slot2_col)
-    print(f"Moving container from {slot1} to {slot2}")
+        slot1_row = computeRow(slot1R) 
+        slot1_col = computeCol(slot1C) 
+        slot1 = (slot1_row, slot1_col)
+
+        slot2_row = computeRow(slot2R) 
+        slot2_col = computeCol(slot2C) 
+        slot2 = (slot2_row, slot2_col)
+
+        print("SLOT1", slot1, "; SLOT2", slot2)
+        print(f"Moving container {name} from {slot1} to {slot2}")
 
 
-    moveMaxHeight = maxY(slot1, slot2)
-    moveMaxDown = maxDown(slot2, moveMaxHeight)
-    print(moveMaxDown)
+        moveMaxHeight = maxY(slot1, slot2)
+        moveMaxDown = maxDown(slot2, moveMaxHeight)
+        print(moveMaxDown)
 
 
-    while True:
-        animateUp(moveMaxHeight, slot1)
-        # root.after(timer)
-        animateHorizontal(slot1, slot2, moveMaxHeight)
-        animateDown(slot2, moveMaxHeight, moveMaxDown)
-        if running == False:  #add condition for when user hits "Next", stop the loop so it can go to next step's new animation
-            break       
+        while True:
+            animateUp(moveMaxHeight, slot1)
+            # root.after(timer)
+            animateHorizontal(slot1, slot2, moveMaxHeight)
+            animateDown(slot2, moveMaxHeight, moveMaxDown)
+            if running == False:  #add condition for when user hits "Next", stop the loop so it can go to next step's new animation
+                updateNextGrid(slot1,slot2)       # TODO: replace "break" with updateNextGrid(slot1,slot2) here
+                on_start()
+                break
+            
     
-    updateNextGrid(slot1,slot2)
+        # #TODO: CHECK IF button_grid IS ALSO UPDATED, NOT ONLY THE GUI GRID RUN FOR LOOP THROUGH BUTTON_GRID AND MAKE A COMPARISON BETWEEN ITS ARRAY CONTENTS AND THEIR POSITIONS BEFORE THE .update() AND AFTER THE .update() <-- YES, it does update
+        # for i in range(len(button_grid)):
+        #     print("ROW", i)
+        #     for j in range(12):
+        #         print("COL",j, button_grid[i][j].cget("bg"))
+        
     print("Broke out of animation loop!") #break out of loop
     root.mainloop()
 
